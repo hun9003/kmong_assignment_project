c9bb09c36d5059c3727861665ecf6d7f2af81f76
hun9003 <hun9003@naver.com>
2022-08-01 19:35:04 +0900

    first commit

---
+++ src/main/java/com/parkjinhun/kmong/kmong_assignment_project/KmongAssignmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.parkjinhun.kmong.kmong_assignment_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmongAssignmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmongAssignmentProjectApplication.class, args);
+    }
+
+}
