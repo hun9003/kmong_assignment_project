8b919c39c095de7fd633ebd0cfd90c51a3267312
hun9003 <hun9003@naver.com>
2022-08-02 23:53:59 +0900

    상품 도메인 계층 구성

---
+++ src/main/java/com/parkjinhun/kmong/kmong_assignment_project/domain/item/optiongroup/ItemOptionGroupStore.java
@@ -0,0 +1,5 @@
+package com.parkjinhun.kmong.kmong_assignment_project.domain.item.optiongroup;
+
+public interface ItemOptionGroupStore {
+    ItemOptionGroup store(ItemOptionGroup itemOptionGroup);
+}
